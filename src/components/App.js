f27befdf0459a8b943ea32d134face88bba2471e
DED8IRD <ewu72829@gmail.com>
2018-11-25 15:24:41 -0800

    set up redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import TodoList from './TodoList'
+import Link from './Link'
+import Filters from './Filters'
+// import {FilterLink, VisibleTodoList} from '../containers'
 
 export default class App extends React.Component {
 	constructor(props) {
